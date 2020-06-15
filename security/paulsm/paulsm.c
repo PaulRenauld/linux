fe11cee1c942f24da0dcb6e59f2d43db06f62014
Paul Renauld <paulrenauld27@gmail.com>
2020-06-15 12:55:34 +0200

    print all hooks

--- security/paulsm/paulsm.c
+++ security/paulsm/paulsm.c
@@ -2,7 +2,7 @@
 #include <linux/module.h>
 #include <linux/lsm_hooks.h>
 
-static int paul_inode_unlink(struct inode *dir, struct dentry *dentry)
+/*static int paul_inode_unlink(struct inode *dir, struct dentry *dentry)
 {
 	if (strncmp("paul", dentry->d_iname, 4) == 0) {
 		printk(KERN_ALERT "Paul's unlinking node alert: %s\n", 
@@ -14,7 +14,26 @@ static int paul_inode_unlink(struct inode *dir, struct dentry *dentry)
 
 static struct security_hook_list paulsm_hooks[] __lsm_ro_after_init = {
 	LSM_HOOK_INIT(inode_unlink, paul_inode_unlink),
+};*/
+
+
+#define LSM_HOOK(RET, DEFAULT, NAME, ...)	\
+noinline RET paulsm_##NAME(__VA_ARGS__)	\
+{						\
+	/*printk(KERN_WARNING "LSM HOOK: %s\n", #NAME);*/ \
+	return DEFAULT;				\
+}
+
+#include <linux/lsm_hook_defs.h>
+#undef LSM_HOOK
+
+static struct security_hook_list paulsm_hooks[] __lsm_ro_after_init = {
+	#define LSM_HOOK(RET, DEFAULT, NAME, ...) \
+	LSM_HOOK_INIT(NAME, paulsm_##NAME),
+	#include <linux/lsm_hook_defs.h>
+	#undef LSM_HOOK
 };
+
 
 static int __init paulsm_init(void)
 {
